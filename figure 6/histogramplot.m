715028623517747c4fa311e04035d22b4375f808
Immunoengineeringlab <87480753+Immunoengineeringlab@users.noreply.github.com>
2025-05-22 15:10:44 +0530

    Update histogramplot.m

--- figure 6/histogramplot.m
+++ figure 6/histogramplot.m
@@ -1,23 +1,23 @@
 fvrpm=load('masterdata_rpm_3inputs.mat','FVALUES')
-fvrpm_3 = fvrpm.FVALUES
+fvrpm_3 = (fvrpm.FVALUES)/1000
 fvhill=load('masterdata_witt_3inputs.mat','FVALUES')
-fvhill_3 = fvhill.FVALUES
+fvhill_3 = (fvhill.FVALUES)/1000
 
 
 subplot(2,1,1)
-histogram(fvrpm,'FaceColor',[240 225 60]/256)
+histogram(fvrpm_3,'FaceColor',[240 225 60]/256)
 xlim
 subplot(2,1,2)
-histogram(fvhill,'FaceColor',[86 180 233]/256) 
+histogram(fvhill_3,'FaceColor',[86 180 233]/256) 
 
 fvrpm=load('masterdata_rpm_5inputs.mat','FVALUES')
-fvrpm_5 = fvrpm.FVALUES
+fvrpm_5 = (fvrpm.FVALUES)/1000
 fvhill=load('masterdata_witt_5inputs.mat','FVALUES')
-fvhill_5 = fvhill.FVALUES
+fvhill_5 = (fvhill.FVALUES)/1000
 
 figure
 subplot(2,1,1)
-histogram(fvrpm,'FaceColor',[240 225 60]/256)
+histogram(fvrpm_5,'FaceColor',[240 225 60]/256)
 xlim
 subplot(2,1,2)
-histogram(fvhill,'FaceColor',[86 180 233]/256)
+histogram(fvhill_5,'FaceColor',[86 180 233]/256)
