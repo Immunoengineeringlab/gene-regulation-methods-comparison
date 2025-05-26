de298802b36f7d01e387cbcf17410b0dc728a374
Immunoengineeringlab <87480753+Immunoengineeringlab@users.noreply.github.com>
2025-05-26 19:39:53 +0530

    Update toyw.m

--- figure 8/8c/train Hillcube/toyw.m
+++ figure 8/8c/train Hillcube/toyw.m
@@ -13,7 +13,7 @@ A = D(1)*(   hillcube1(n{1},K{1},xc,BM{1},F{1})      - y(1));  %differential equ
 
 xc=[y(1),y(2),y(3)];
 
-B = D(2)*(    hillcube1(n{2},K{2},xc,BM{2},F{2})     - y(3));  %differential equation for node B
+B = D(2)*(    hillcube1(n{2},K{2},xc,BM{2},F{2})     - y(2));  %differential equation for node B
 
 
 
@@ -23,7 +23,7 @@ B = D(2)*(    hillcube1(n{2},K{2},xc,BM{2},F{2})     - y(3));  %differential equ
 
 xc=[y(1),y(2),y(3),I(1),I(2)];
 
-C= D(3)*(     hillcube1(n{3},K{3},xc,BM{3},F{3})     - y(5));  %differential equation for node C
+C= D(3)*(     hillcube1(n{3},K{3},xc,BM{3},F{3})     - y(3));  %differential equation for node C
  
 
 
